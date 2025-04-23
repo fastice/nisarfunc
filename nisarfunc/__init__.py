a7b7f62e1a7f4df8c8a7d81957fe2eff65dcfe34
fastice <47194958+fastice@users.noreply.github.com>
2025-04-23 14:25:40 -0700

    Update __init__.py

--- nisarfunc/__init__.py
+++ nisarfunc/__init__.py
@@ -1,11 +1,21 @@
 __all__ = ['setKey', 'readGeoTiff', 'myError', 'parseDatesFromDirName',
            'parseDatesFromMeta',
-           'cvPoints', 'nisarBase2D', 'nisarVel']
-
+           'cvPoints', 'nisarBase2D', 'nisarVel',
+           'nccPatches', 'correlatedShiftedPatches', 'speckleJobs',
+           'osSubPix', 'osSubPixGaussian', 'gaussFit']
 
 from nisarfunc.nisarSupport import setKey, myError, readGeoTiff
 from nisarfunc.nisarSupport import parseDatesFromDirName, parseDatesFromMeta
 from nisarfunc.cvPoints import cvPoints
 from nisarfunc.nisarBase2D import nisarBase2D
 from nisarfunc.nisarVel import nisarVel
+from nisarfunc.speckleSim import overSamplePatch
+from nisarfunc.speckleSim import patchShift
+from nisarfunc.speckleSim import nccIntPatches
+from nisarfunc.speckleSim import nccPatches
+from nisarfunc.speckleSim import correlatedShiftedPatches
+from nisarfunc.speckleSim import speckleJobs
+from nisarfunc.speckleSim import osSubPix
+from nisarfunc.speckleSim import osSubPixGaussian
+from nisarfunc.speckleSim import gaussFit
 
